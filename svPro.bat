dec7d5ab41f2a0bc583321bfb15bfd14bc7e313e
Ferran Salguero Mateu <fsalgueromateu@gmail.com>
2026-01-17 13:36:17 +0100

    review qof

--- svPro.bat
+++ svPro.bat
@@ -1,4 +1,5 @@
 @echo off
 echo Starting Jekyll with LOCAL theme (fast mode)...
 set BUNDLE_GEMFILE=Gemfile.local
+bundle install --quiet 2>nul
 bundle exec jekyll serve -I --config _config.yml,_config_dev.yml
